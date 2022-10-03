01c7350f2cf04605a2a5ba81529a61e264b541c9
vascYT <nicovanaschaufler@gmail.com>
2022-10-03 19:27:49 +0200

    add /donate redirect

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ module.exports = {
 				destination: "https://discord.gg/G58HZKGjv2",
 				permanent: false,
 			},
+			{
+				source: "/donate",
+				destination: "https://www.buymeacoffee.com/vasc",
+				permanent: false,
+			},
 		];
 	},
 };
