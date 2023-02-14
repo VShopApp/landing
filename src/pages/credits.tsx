3a79d6beda05134032e9ae6091912945941bab39
vascYT <nicovanaschaufler@gmail.com>
2023-02-14 16:55:13 +0100

    feat: credits page syncs with discord

--- src/pages/credits.tsx
+++ src/pages/credits.tsx
@@ -1,163 +1,125 @@
 import PageLayout from "../components/PageLayout";
+import { APIUser } from "discord-api-types/v10";
 
+const DISCORD_API_URL = "https://discord.com/api/v10";
 const credits = [
 	{
 		name: "Mobile app",
-		members: [
-			{
-				name: "vasc",
-				imgUrl: "https://avatars.githubusercontent.com/u/39592306",
-			},
-		],
+		users: [{ discordId: "346977366569910274" }],
 	},
 	{
 		name: "Website",
-		members: [{ name: "vasc", imgUrl: "https://avatars.githubusercontent.com/u/39592306" }],
+		users: [{ discordId: "346977366569910274" }],
 	},
 	{
 		name: "Documentation",
-		members: [{ name: "MuteBefehl", imgUrl: "https://avatars.githubusercontent.com/u/36934652" }],
+		users: [{ discordId: "224270178836283392" }],
 	},
 	{
 		name: "Resources",
-		members: [
-			{
-				name: "techchrism",
-				info: "Creator of valorant-api-docs",
-				imgUrl: "https://avatars.githubusercontent.com/u/26680599",
-			},
-			{
-				name: "Not0fficer",
-				info: "Creator of valorant-api.com",
-				imgUrl: "https://avatars.githubusercontent.com/u/29897990",
-			},
+		users: [
+			{ discordId: "239625893738315776", info: "Creator of valorant-api-docs" },
+			{ discordId: "373913699943186432", info: "Creator of valorant-api.com" },
 		],
 	},
 	{
 		name: "Translators",
-		members: [
-			{
-				name: "MuteBefehl",
-				info: "German",
-				imgUrl: "https://avatars.githubusercontent.com/u/36934652",
-			},
-			{
-				name: "C0taro",
-				info: "German",
-				imgUrl: "/img/avatars/707488849420615751.jpg",
-			},
-			{
-				name: "ReyyZero",
-				info: "Chinese Simplified & Traditional",
-				imgUrl: "/img/avatars/413020881246355457.jpg",
-			},
-			{
-				name: "Ban",
-				info: "Arabic",
-				imgUrl: "/img/avatars/643500735031541760.jpg",
-			},
-			{
-				name: "Hoffo",
-				info: "Italian",
-				imgUrl: "/img/avatars/774992489945890896.jpg",
-			},
-			{
-				name: "Mr Schneckenmann",
-				info: "Italian",
-				imgUrl: "/img/avatars/889070107693834251.jpg",
-			},
-			{
-				name: "Enzo Ferrari",
-				info: "Italian",
-				imgUrl: "/img/avatars/942000444912324618.jpg",
-			},
-			{
-				name: "Jennet",
-				info: "Korean",
-				imgUrl: "/img/avatars/901134108221128725.jpg",
-			},
-			{
-				name: "Mokdori_Dambi",
-				info: "Korean",
-				imgUrl: "/img/avatars/366377379599351809.jpg",
-			},
-			{
-				name: "mrespalier",
-				info: "Korean",
-				imgUrl: "/img/avatars/690008685946208323.jpg",
-			},
-			{
-				name: "strumyk;x",
-				info: "Polish",
-				imgUrl: "/img/avatars/284350640279257091.jpg",
-			},
-			{
-				name: "Ivailo",
-				info: "Portuguese",
-				imgUrl: "/img/avatars/395347408243916802.jpg",
-			},
-			{
-				name: "poke",
-				info: "Portuguese",
-				imgUrl: "/img/avatars/default.png",
-			},
-			{
-				name: "Hidy",
-				info: "Russian",
-				imgUrl: "/img/avatars/531840835025764383.jpg",
-			},
-			{
-				name: "GARN",
-				info: "Thai",
-				imgUrl: "/img/avatars/880312739300536331.jpg",
-			},
-			{
-				name: "Kaanimsi",
-				info: "Turkish",
-				imgUrl: "/img/avatars/910503529364471889.jpg",
-			},
-			{
-				name: "Chí Kiên",
-				info: "Vietnamese",
-				imgUrl: "/img/avatars/504207204644290570.jpg",
-			},
-			{
-				name: "damnlinh",
-				info: "Vietnamese",
-				imgUrl: "/img/avatars/813389418349985833.jpg",
-			},
-			{
-				name: "Shynoku",
-				info: "Japanese",
-				imgUrl: "/img/avatars/517401133384466461.jpg",
-			},
+		users: [
+			{ discordId: "224270178836283392", info: "German" },
+			{ discordId: "707488849420615751", info: "German" },
+			{ discordId: "413020881246355457", info: "Chinese Simplified & Traditional" },
+			{ discordId: "504207204644290570", info: "French" },
+			{ discordId: "812814570988503141", info: "French" },
+			{ discordId: "690008685946208323", info: "Korean" },
+			{ discordId: "901134108221128725", info: "Korean" },
+			{ discordId: "366377379599351809", info: "Korean" },
+			{ discordId: "395347408243916802", info: "Portuguese" },
+			{ discordId: "744189530470350919", info: "Portuguese" },
+			{ discordId: "531840835025764383", info: "Russian" },
+			{ discordId: "1069385327418024037", info: "Turkish" },
+			{ discordId: "643500735031541760", info: "Arabic" },
+			{ discordId: "284350640279257091", info: "Polish" },
+			{ discordId: "942000444912324618", info: "Italian" },
+			{ discordId: "889070107693834251", info: "Italian" },
+			{ discordId: "774992489945890896", info: "Italian" },
+			{ discordId: "517401133384466461", info: "Japanese" },
+			{ discordId: "330593196012863490", info: "Spanish" },
+			{ discordId: "880312739300536331", info: "Thai" },
+			{ discordId: "504207204644290570", info: "Vietnamese" },
+			{ discordId: "813389418349985833", info: "Vietnamese" },
 		],
 	},
 ];
 
-export default function Credits() {
+interface IFullCredit {
+	name: string;
+	users: {
+		discordId: string;
+		info?: string;
+		username: string;
+		discriminator: string;
+		avatar: string | null;
+	}[];
+}
+export async function getStaticProps() {
+	const fullCredits: IFullCredit[] = [];
+	for (let i = 0; i < credits.length; i++) {
+		fullCredits[i] = {
+			name: credits[i].name,
+			users: await Promise.all(
+				credits[i].users.map(async user => {
+					const res = await fetch(`${DISCORD_API_URL}/users/${user.discordId}`, {
+						headers: { Authorization: `Bot ${process.env.BOT_TOKEN}` },
+					});
+					const discordUser: APIUser = await res.json();
+					return {
+						username: discordUser.username,
+						discriminator: discordUser.discriminator,
+						avatar: discordUser.avatar,
+						...user,
+					};
+				})
+			),
+		};
+	}
+
+	return {
+		props: {
+			fullCredits,
+		},
+		revalidate: 60 * 60,
+	};
+}
+
+export default function Credits({ fullCredits }: { fullCredits: IFullCredit[] }) {
 	return (
 		<>
 			<PageLayout pageTitle="Credits">
 				<div>
-					{credits.map((item, i) => (
+					{fullCredits.map((item, i) => (
 						<div key={i}>
 							<h1 className="text-2xl font-bold">
 								{item.name}{" "}
-								<span className="text-gray-300 font-normal text-xl">({item.members.length})</span>
+								<span className="text-gray-300 font-normal text-xl">({item.users.length})</span>
 							</h1>
 							<div className="my-4">
-								{item.members.map((member, i) => (
+								{item.users.map((user, i) => (
 									<div key={i} className="flex items-center space-x-2 py-1">
 										<img
-											alt={`${member.name}'s profile picture`}
-											src={member.imgUrl}
+											alt={`${user.username}'s profile picture`}
+											src={
+												user.avatar
+													? `https://cdn.discordapp.com/avatars/${user.discordId}/${user.avatar}.png?size=64`
+													: `https://cdn.discordapp.com/embed/avatars/${
+															Number.parseInt(user.discriminator) % 5
+													  }.png`
+											}
 											className="w-10 rounded-full"
 										/>
 										<div>
-											<p className="text-lg">{member.name}</p>
-											{(member as any).info && (
-												<p className="text-xs text-gray-300">{(member as any).info}</p>
+											<p className="text-lg">{user.username}</p>
+											{(user as any).info && (
+												<p className="text-xs text-gray-300">{(user as any).info}</p>
 											)}
 										</div>
 									</div>
