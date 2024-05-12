4836089e4b3af808977fe381371f9f1c7c04b994
vasc <nico@vasc.dev>
2024-05-12 19:39:42 +0200

    use static build

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
+/** @type {import("next").NextConfig} */
 module.exports = {
-	async redirects() {
-		return [
-			{
-				source: "/discord",
-				destination: "https://discord.gg/G58HZKGjv2",
-				permanent: false,
-			},
-			{
-				source: "/restore-purchase",
-				destination: "https://tally.so/r/mRdPEJ",
-				permanent: false,
-			},
-		];
-	},
+	output: "export",
 };
